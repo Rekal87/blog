53f3f5b6156ebb33dc78e2ad9923a604d8fadb87
Nnamdi Echem <nnamdi.b@gmail.com>
2025-10-29 23:21:24 +0100

    added useState, useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Homepage from "./components/Homepage";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await res.json();
+      setPosts(data);
+    };
+
+    fetchPosts();
+  });
+
   return (
     <div className="App">
-      <Navbar />
-      <div className="content">
-        <Homepage />
+      <div className="bg-gray-900 py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl">
+              the Blog
+            </h2>
+            <p className="mt-2 text-lg/8 text-gray-300">
+              Learn how to grow your business with our expert advice.
+            </p>
+          </div>
+          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-700 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3"></div>
+        </div>
       </div>
     </div>
   );
